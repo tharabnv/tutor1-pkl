1ff58f93127c89c9e9957d7793eb64f6ee3cf988
tharbnv <tharabnv@gmail.com>
2025-06-01 00:00:26 +0700

    update baru banget

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiGuruController;
+use App\Http\Controllers\ApiSiswaController;
+use App\Http\Controllers\ApiIndustriController;
+use App\Http\Controllers\ApiPklController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource("guru", ApiGuruController::class);
+Route::apiResource("siswa", ApiSiswaController::class);
+Route::apiResource("industri", ApiIndustriController::class);
+Route::apiResource("pkl", ApiPklController::class);
